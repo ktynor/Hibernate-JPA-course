bb8976c843c2809a31f7941506bdb895e4b74f36
ktynor <kt9@tlen.pl>
2021-01-16 12:48:34 +0000

    update product in DB

---
+++ src/main/java/pl/nullpointerexception/hibernate/entity/App03Update.java
@@ -0,0 +1,27 @@
+package pl.nullpointerexception.hibernate.entity;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pl.nullpointerexception.hibernate.App;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class App03Update {
+    private static Logger logger = LogManager.getLogger(App.class);
+    private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+
+    public static void main(String[] args) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+        Product product = em.find(Product.class, 1L);
+        product.setName("Nowy Rower");
+        Product merged = em.merge((product));
+        logger.info(product);
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
