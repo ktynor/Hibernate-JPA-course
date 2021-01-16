a82cd0c041df4fa18dc4e628045353ecca61a459
ktynor <kt9@tlen.pl>
2021-01-16 11:01:18 +0000

    new App01Create to create and save entity

--- src/main/java/App01Create.java
+++ src/main/java/App01Create.java
@@ -5,8 +5,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-
-public class App {
+public class App01Create {
 
     private static Logger logger = LogManager.getLogger(App.class);
     private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
@@ -14,7 +13,7 @@ public class App {
     public static void main(String[] args) {
         EntityManager em = entityManagerFactory.createEntityManager();
         em.getTransaction().begin();
-        logger.info("Hello!");
+
         em.getTransaction().commit();
         em.close();
     }
