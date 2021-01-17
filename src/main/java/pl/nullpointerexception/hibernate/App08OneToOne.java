d669342907c8c29560a40be463a20ccd00f83290
ktynor <kt9@tlen.pl>
2021-01-17 20:30:23 +0000

    mapping @OneToOne Product and Category

--- src/main/java/pl/nullpointerexception/hibernate/App08OneToOne.java
+++ src/main/java/pl/nullpointerexception/hibernate/App08OneToOne.java
@@ -2,6 +2,7 @@ package pl.nullpointerexception.hibernate;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import pl.nullpointerexception.hibernate.entity.Product;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -16,6 +17,9 @@ public class App08OneToOne {
         EntityManager em = entityManagerFactory.createEntityManager();
         em.getTransaction().begin();
 
+        Product product = em.find(Product.class, 3L);
+        logger.info(product);
+        logger.info(product.getCategory().getName());
 
         em.getTransaction().commit();
         em.close();
