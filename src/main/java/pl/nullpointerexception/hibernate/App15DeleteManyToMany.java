8c72dea0c417b992b1aabcc66943f3d1fc6083a4
ktynor <kt9@tlen.pl>
2021-01-20 22:30:43 +0000

    removing attributes by removing product from attributes first

--- src/main/java/pl/nullpointerexception/hibernate/App15DeleteManyToMany.java
+++ src/main/java/pl/nullpointerexception/hibernate/App15DeleteManyToMany.java
@@ -2,12 +2,14 @@ package pl.nullpointerexception.hibernate;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import pl.nullpointerexception.hibernate.entity.Attribute;
 import pl.nullpointerexception.hibernate.entity.Product;
 
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.ArrayList;
 
 public class App15DeleteManyToMany {
 
@@ -18,9 +20,19 @@ public class App15DeleteManyToMany {
         EntityManager em = entityManagerFactory.createEntityManager();
         em.getTransaction().begin();
 
-        Product product=em.find(Product.class, 5L);
-//        em.remove(product);
-        product.getAttributes().clear();//clears set attributes
+//        Product product=em.find(Product.class, 5L);
+////        em.remove(product);
+//        product.getAttributes().clear();//clears set attributes
+
+        Attribute attribute = em.find(Attribute.class, 1L);
+        //below new ArrayList to avoid ConcurrentModificationException while removing items from set
+        for (Product product : new ArrayList<>(attribute.getProducts())) {
+            attribute.removeProduct(product);
+        }
+        for (Product product : new ArrayList<>(attribute.getProducts())) {
+            attribute.removeProduct(product);
+        }
+        em.remove(attribute);
 
         em.getTransaction().commit();
         em.close();
