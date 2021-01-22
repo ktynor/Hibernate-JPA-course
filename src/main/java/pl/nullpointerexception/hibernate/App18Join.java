88120cc7bb9953c6eff52ac6bda420f3388e8cac
ktynor <kt9@tlen.pl>
2021-01-22 09:06:42 +0000

    select p from Product p inner join p.category c where c.id=:id

---
+++ src/main/java/pl/nullpointerexception/hibernate/App18Join.java
@@ -0,0 +1,36 @@
+package pl.nullpointerexception.hibernate;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pl.nullpointerexception.hibernate.entity.Product;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class App18Join {
+
+    private static Logger logger = LogManager.getLogger(App18Join.class);
+    private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+
+    public static void main(String[] args) {
+
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+        TypedQuery<Product> query = em.createQuery("select p from Product p " +
+                "inner join p.category c " +
+                "where c.id=:id", Product.class);
+        query.setParameter("id", 1L);
+        List<Product> resultList = query.getResultList();
+        for (Product product : resultList) {
+            logger.info(product.getName());
+            logger.info(product.getCategory().getName());
+        }
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
