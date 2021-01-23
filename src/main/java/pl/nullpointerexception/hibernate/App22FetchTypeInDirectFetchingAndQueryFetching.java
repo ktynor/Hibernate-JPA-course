bde8413f567432a5cd6f00f9332026e0ce5aa562
ktynor <kt9@tlen.pl>
2021-01-23 10:05:33 +0000

    //@OneToOne(fetch = FetchType.LAZY) //second query to get category

---
+++ src/main/java/pl/nullpointerexception/hibernate/App22FetchTypeInDirectFetchingAndQueryFetching.java
@@ -0,0 +1,33 @@
+package pl.nullpointerexception.hibernate;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pl.nullpointerexception.hibernate.entity.Product;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class App22FetchTypeInDirectFetchingAndQueryFetching {
+
+    private static Logger logger = LogManager.getLogger(App22FetchTypeInDirectFetchingAndQueryFetching.class);
+    private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+
+    public static void main(String[] args) {
+
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+        Product product = em.createQuery(
+                "select p from Product p where p.id=:id",
+                Product.class)
+                .setParameter("id", 1L)
+                .getSingleResult();
+        logger.info(product);
+        logger.info(product.getCategory()); //@OneToOne(fetch = FetchType.LAZY)
+                                            //second query to get category
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
