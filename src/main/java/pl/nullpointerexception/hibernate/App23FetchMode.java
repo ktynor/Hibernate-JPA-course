0ea1b351a98c7193c7b6fc78189df6fb48be2c7a
ktynor <kt9@tlen.pl>
2021-01-25 11:53:03 +0000

    // @Fetch(FetchMode.JOIN) // many additional queries for each orderRow in a LAZY way

--- src/main/java/pl/nullpointerexception/hibernate/App23FetchMode.java
+++ src/main/java/pl/nullpointerexception/hibernate/App23FetchMode.java
@@ -9,6 +9,7 @@ import pl.nullpointerexception.hibernate.entity.Order;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class App23FetchMode {
 
@@ -20,10 +21,15 @@ public class App23FetchMode {
         EntityManager em = entityManagerFactory.createEntityManager();
         em.getTransaction().begin();
 
-        Order order = em.find(Order.class, 1L);
-        logger.info(order);
-        logger.info(order.getOrderRows()); // @Fetch(FetchMode.SUBSELECT)
-// Results same as before, SUBSELECT will create additional query when we use 'select' query instead of 'find'
+//        Order order = em.find(Order.class, 1L);
+        List<Order> orders = em.createQuery("select o from Order o", Order.class
+        ).getResultList();
+        for (Order order : orders) {
+            logger.info(order);
+            logger.info(order.getOrderRows());
+        }
+         // @Fetch(FetchMode.JOIN)
+// many additional queries for each orderRow in a LAZY way
 
         em.getTransaction().commit();
         em.close();
--- src/main/java/pl/nullpointerexception/hibernate/entity/Order.java
+++ src/main/java/pl/nullpointerexception/hibernate/entity/Order.java
@@ -19,7 +19,7 @@ public class Order {
 
     @OneToMany
     @JoinColumn(name = "order_id")
-    @Fetch(FetchMode.SUBSELECT)
+    @Fetch(FetchMode.JOIN)
     private Set<OrderRow> orderRows;
 
     public Long getId() {
